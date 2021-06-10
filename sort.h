af02d7271ccbeaeb7f26620c0f9da3481bab2a19
root <s_borgia@hotmail.com>
2021-06-10 22:40:51 +0000

    added base files and header

---
+++ sort.h
@@ -0,0 +1,53 @@
+#ifndef SORT_H
+#define SORT_H
+
+/*including libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <stddef.h>
+#include <time.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+/*Basic Structure*/
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+  const int n;
+  struct listint_s *prev;
+  struct listint_s *next;
+
+} listint_t;
+
+/*Prototypes*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+/*Advanced Prototypes*/
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/*EXTRA*/
+void sort_deck(deck_node_t **deck);
+
+#endif /*ends SORT_H*/
