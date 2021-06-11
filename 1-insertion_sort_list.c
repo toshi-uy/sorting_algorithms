bcdfe641a7c52944e4615d03b9e503868d30e007
RaimundoGallino <raigallino0@gmail.com>
2021-06-11 20:02:21 -0300

    Updating swap

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -22,16 +22,20 @@ void insertion_sort_list(listint_t **list)
 		{
 			tmp1 = h;
 			if(h->next)
+				printf("entro 1")
 				tmp2 = h->next;
 			
 			if(tmp1->prev)
+				printf("entro2")
 				tmp1->prev->next = tmp2;
 			if(tmp2->next)
+				printf("entro 3")
 				tmp2->next->prev = tmp1;
-			
+			printf("cambiando tmp2 y tmp1")
 			tmp2->prev = tmp1->prev;
 			tmp1->next = tmp2->next;
 			
+			printf("cambiando prev y next")
 			tmp1->prev = tmp2;
 			tmp2->next = tmp1;
 
@@ -39,12 +43,16 @@ void insertion_sort_list(listint_t **list)
 			{
 				tmp1 = h2;
 				
+				printf("entro al while")
 				if(h2->next)
+					printf("entro 4")
 					tmp2 = h2->next;
 				
 				if(tmp1->prev)
+					printf("entro 5")
 					tmp1->prev->next = tmp2;
 				if(tmp2->next)
+					printf("entro 6")
 					tmp2->next->prev = tmp1;
 				
 				tmp2->prev = tmp1->prev;
