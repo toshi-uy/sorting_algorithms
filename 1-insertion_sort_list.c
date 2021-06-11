ff7cc8def64b4dcaab416cb74dd5566c5b9cb665
RaimundoGallino <raigallino0@gmail.com>
2021-06-11 20:50:54 -0300

    Updating swap

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,70 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
  * insertion_sort_list - this is the Bubble sort algorithm implementation in C
- * @list: array of ints
+ * @array: array of ints
+ * @size: size of array
  * Return: no-return (void function)
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *h2 = NULL, *tmp1 = NULL, *tmp2 = NULL;
-	listint_t *h = *list;
-	
-	write(2, "entro 1\n", 8);
+    listint_t *h = NULL, *h2 = NULL, *tmp1 = NULL, *tmp2 = NULL;
 
-	while(h)
+	h = *list;
+	while(h->next)
 	{
 		h2 = h;
-		if (h->next->n < h->n)
+		if(h->next)
 		{
-			tmp1 = h;
-			if(h->next)
-				printf("entro 1");
-				tmp2 = h->next;
-			
-			if(tmp1->prev)
-				printf("entro2");
-				tmp1->prev->next = tmp2;
-			if(tmp2->next)
-				printf("entro 3");
-				tmp2->next->prev = tmp1;
-			printf("cambiando tmp2 y tmp1");
-			tmp2->prev = tmp1->prev;
-			tmp1->next = tmp2->next;
-			
-			printf("cambiando prev y next");
-			tmp1->prev = tmp2;
-			tmp2->next = tmp1;
-
-			while(h2->prev && h2->prev->n > h2->n)
+			if (h->next->n < h->n)
 			{
-				tmp1 = h2;
-				
-				printf("entro al while");
-				if(h2->next)
-					printf("entro 4");
-					tmp2 = h2->next;
-				
-				if(tmp1->prev)
-					printf("entro 5");
-					tmp1->prev->next = tmp2;
-				if(tmp2->next)
-					printf("entro 6");
-					tmp2->next->prev = tmp1;
-				
-				tmp2->prev = tmp1->prev;
-				tmp1->next = tmp2->next;
-				
-				tmp1->prev = tmp2;
-				tmp2->next = tmp1;
+				tmp1 = h;
+				if (tmp1->prev)
+						tmp1->prev->next = tmp1->next;
+				if (tmp1->next->next)
+						tmp1->next->next->prev = tmp1;
 
-				h2 = h2->prev;
+				tmp1->next->prev = tmp1->prev;
+				tmp1->prev = tmp1->next;
+				if (tmp1->next->next)
+					tmp2 = tmp1->next->next;
+				tmp1->next->next = tmp1;
+				tmp1->next = tmp2;
+
+
+
+				while(h2->prev && h2->prev->n > h2->n)
+				{
+					tmp1 = h2;
+					if (tmp1->prev)
+						tmp1->prev->next = tmp1->next;
+					if (tmp1->next->next)
+						tmp1->next->next->prev = tmp1;
+
+					tmp1->next->prev = tmp1->prev;
+					tmp1->prev = tmp1->next;
+					if (tmp1->next->next)
+						tmp2 = tmp1->next->next;
+					tmp1->next->next = tmp1;
+					tmp1->next = tmp2;
+
+					h2 = h2->prev;
+				}
 			}
-		}
-		if (h->next)
 			h = h->next;
-	}
+		}
+    }
 }
 
 
@@ -75,15 +67,16 @@ void insertion_sort_list(listint_t **list)
  * if(h2->next)
  *      tmp2 = h2->next
  * 
+ * 
  * if(tmp1->prev)
  *      tmp1->prev->next = tmp2
  * if(tmp2->next)
  *      tmp2->next->prev = tmp1
  * 
- * tmp2->prev = tmp1->prev
+ * tmp1->prev = tmp2
  * tmp1->next = tmp2->next
  * 
- * tmp1->prev = tmp2
+ * tmp2->prev = tmp1->prev
  * tmp2->next = tmp1
  * 
  * 
