fad0f82f8465986140ac3eebe6dc5bb6e9ec4ea9
RaimundoGallino <raigallino0@gmail.com>
2021-06-12 13:49:29 -0300

    Updating swap

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,50 +13,49 @@ void insertion_sort_list(listint_t **list)
 	h = *list;
 	while(h->next)
 	{
-		if(h->next)
+		printf("entro al primer while\n");
+		if (h->next->n < h->n)
 		{
-			if (h->next->n < h->n)
+			tmp1 = h;
+
+			if(h->next)
+				tmp2 = h->next;
+			if(tmp1->prev)
+				tmp1->prev->next = tmp2;
+			if(tmp2 && tmp2->next)
+				tmp2->next->prev = tmp1;
+
+			tmp1->prev = tmp2;
+			tmp1->next = tmp2->next;
+
+			tmp2->prev = tmp1->prev;
+			tmp2->next = tmp1;
+			print_list(*list);
+			
+			h2 = h->prev;
+			if (h2->prev)
 			{
-				tmp1 = h;
-
-				if(h->next)
-					tmp2 = h->next;
-				if(tmp1->prev)
-					tmp1->prev->next = tmp2;
-				if(tmp2 && tmp2->next)
-					tmp2->next->prev = tmp1;
-
-				tmp1->prev = tmp2;
-				tmp1->next = tmp2->next;
-
-				tmp2->prev = tmp1->prev;
-				tmp2->next = tmp1;
-				print_list(*list);
-				
-				h2 = h->prev;
-				if (h2->prev)
+				while(h2->prev->n > h2->n)
 				{
-					while(h2->prev->n > h2->n)
-					{
-						tmp1 = h2;
-						printf("entro al while\n");
-						tmp2 = h2->prev;
-						if (tmp2->prev)
-							tmp2->prev->next = tmp1;
+					tmp1 = h2;
+					printf("entro al while\n");
+					tmp2 = h2->prev;
+					if (tmp2->prev)
+						tmp2->prev->next = tmp1;
 
-						tmp1->next->prev = tmp2;
+					tmp1->next->prev = tmp2;
 
-						tmp2->next = tmp1->next;
-						tmp1->prev = tmp2->prev;
-						
-						tmp2->prev = tmp1;
-						tmp1->next = tmp2;
-						print_list(*list);
-					}
+					tmp2->next = tmp1->next;
+					tmp1->prev = tmp2->prev;
+					
+					tmp2->prev = tmp1;
+					tmp1->next = tmp2;
+					print_list(*list);
 				}
 			}
-			else
-				h = h->next;
+		}
+		else
+			h = h->next;
 		}
 	}
 }
