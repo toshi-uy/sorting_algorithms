6a23e3e66b2accdbe94a6a5993c54c5b39374f7c
toshi-uy <s_borgia@hotmail.com>
2021-06-13 13:11:17 -0300

    max = i on every for

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,6 +14,7 @@ void selection_sort(int *array, size_t size)
 
     for (i = 0; i < size - 1; i++)
     {
+        max = i;
         for (j = 0; j < size; j++)
         {
             if (array[j] > array[max])
