f73fe78e0d77dab36872d3594ad7ff48d0abe825
toshi-uy <s_borgia@hotmail.com>
2021-06-13 13:07:22 -0300

    test always i = 0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,12 +9,12 @@
 
 void selection_sort(int *array, size_t size)
 {
-    int max = 0, tmp = 0;
+    int min = 0, max = 0, tmp = 0;
     size_t i = 0, j = 0;
 
-    for (i = 0; i < size - 1; i++)
+    for (; i < size - 1; i++)
     {
-        max = i;
+        i = 0;
         for (j = i + 1; j < size; j++)
         {
             if (array[j] > array[max])
