2d11965b6148676b1d2516c6d19026dc39514cf2
toshi-uy <s_borgia@hotmail.com>
2021-06-13 20:03:28 -0300

    testing <= size

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,19 +9,18 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t min = 0, tmp = 0, i = 0, j = 0;
+	size_t min = 0, i = 0, j = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j <= size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
 		}
-			tmp = array[min];
-			array[min] = array[i];
-			array[i] = tmp;
+			swap(array, min, i);
+
 			if (min != i)
 				print_array(array, size);
 	}
