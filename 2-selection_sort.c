c06cff5383f64648dafcf9a41f1d9b6076e9e30f
toshi-uy <s_borgia@hotmail.com>
2021-06-13 13:45:27 -0300

    betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,20 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t min = 0, tmp = 0, i = 0, j = 0;
+	size_t min = 0, tmp = 0, i = 0, j = 0;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-        tmp = array[min];
-        array[min] = array[i];
-        array[i] = tmp;
-        if (min != i)
-            print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			if (min != i)
+				print_array(array, size);
+	}
 }
