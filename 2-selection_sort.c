aa2bb0574e1384dcaca7430f2f6a73b233292841
toshi-uy <s_borgia@hotmail.com>
2021-06-13 12:15:09 -0300

    testing sort alog

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - this is the Selection sort algorithm implementation in C
+ * @array: array of ints
+ * @size: size of array
+ * Return: no-return (void function)
+ */
+
+void selection_sort(int *array, size_t size)
+{
+    int max = 0, tmp = 0;
+    size_t i = 0, j = 0;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        max = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[max])
+                max = j;
+        }
+        tmp = array[i];
+        array[i] = max;
+        max = tmp;
+    }
+}
