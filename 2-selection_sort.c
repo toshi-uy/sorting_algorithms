33296bc2982d3c810aef077407466d96705d6aff
toshi-uy <s_borgia@hotmail.com>
2021-06-13 13:29:01 -0300

    not python toshi

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,6 +23,6 @@ void selection_sort(int *array, size_t size)
         tmp = array[min];
         array[min] = array[i];
         array[i] = tmp;
-        print_array(array, size)
+        print_array(array, size);
     }
 }
