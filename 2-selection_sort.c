0ccaf99a34f301dbf1ed857e4fc61f8b59242fb3
toshi-uy <s_borgia@hotmail.com>
2021-06-13 20:05:30 -0300

    test1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j <= size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
