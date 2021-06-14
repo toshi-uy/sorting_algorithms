853483c204ab9f03d6e93ba04feda46baa90087a
toshi-uy <s_borgia@hotmail.com>
2021-06-14 12:25:42 -0300

    revert changes

--- swap.c
+++ swap.c
@@ -57,12 +57,11 @@ void swap_back(listint_t *head, listint_t **list)
 	tmp1 = head;
 	if (tmp2->prev)
 		tmp2->prev->next = tmp1;
-		tmp1->prev = tmp2->prev;
 	tmp1->next->prev = tmp2;
 	tmp2->next = tmp1->next;
+	tmp1->prev = tmp2->prev;
 	tmp2->prev = tmp1;
 	tmp1->next = tmp2;
 	if (!tmp1->prev)
-		tmp1->prev = NULL;
 		*list = tmp1;
 }
