719bdd47c2ab413001c02a9b5ff9f9826177ba23
toshi-uy <s_borgia@hotmail.com>
2021-06-14 11:00:15 -0300

    changed && for ||

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,7 +13,7 @@ void insertion_sort_list(listint_t **list)
 
 	if (!list || !(*list) || !(*list)->next)
 		return;
-	if (list && *list && (*list)->next)
+	if (list || *list || (*list)->next)
 	{
 		while (h->next)
 		{
