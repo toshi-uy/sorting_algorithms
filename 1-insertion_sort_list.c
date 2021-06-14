a65bf4cdf7cc7f9c45156f36fd388471ca219840
toshi-uy <s_borgia@hotmail.com>
2021-06-14 11:03:59 -0300

    change back to check

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,11 +9,10 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *h, *h2 = NULL;
+	listint_t *h = *list, *h2 = NULL;
 
 	if (!list || !(*list) || !(*list)->next)
 		return;
-	h = *list;
 	while (h->next)
 	{
 		if (h->next->n < h->n)
