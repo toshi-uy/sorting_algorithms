41dbb1c63670ba8a42ea8a07a10311cf67f89cf5
toshi-uy <s_borgia@hotmail.com>
2021-06-14 12:27:08 -0300

    added conditions

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -20,11 +20,14 @@ void insertion_sort_list(listint_t **list)
 		{
 			swap_forward(h);
 			print_list(*list);
-			h2 = h->prev;
-			while (h2->prev && h2->n < h2->prev->n)
+			if (h->prev)
 			{
-				swap_back(h2, list);
-				print_list(*list);
+				h2 = h->prev;
+				while (h2->prev && h2->n < h2->prev->n)
+				{
+					swap_back(h2, list);
+					print_list(*list);
+				}
 			}
 		}
 		else
