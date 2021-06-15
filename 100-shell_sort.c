f1c689def7499a373e99d122893c1afb38d3ffb8
toshi-uy <s_borgia@hotmail.com>
2021-06-15 19:17:11 -0300

    testing gap changes

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,7 +10,7 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i = 0, j = 0, k = 0, inicio = 0;
+	size_t gap = 1, i = 0, j = 0, k = 0;
 
 	if (!array || size < 2)
 		return;
@@ -28,7 +28,7 @@ void shell_sort(int *array, size_t size)
 				{
 					swap(array, j, j + gap);
 					k = j;
-					while (k - gap >= inicio && array[k] < array[k - gap])
+					while (k < gap && array[k] < array[k - gap])
 					{
 						swap(array, k, k - gap);
 						k -= gap;
@@ -38,6 +38,6 @@ void shell_sort(int *array, size_t size)
 			}
 		}
 		print_array(array, size);
-		gap /= 3;
+		gap = (gap - 1) / 3;
 	}
 }
