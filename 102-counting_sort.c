6de095fd701673f1859e61c8622ea94accd88395
toshi-uy <s_borgia@hotmail.com>
2021-06-16 15:39:39 -0300

    testing aux

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,15 +12,18 @@
 void counting_sort(int *array, size_t size)
 {
 	unsigned int i;
-	int j, k, size_i = 0, *index, biggest = 0, count = 0;
+	int j, k, size_i = 0, *index, *aux, biggest = 0, count = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		if (biggest < array[i])
 			biggest = array[i];
 	}
+	aux = malloc(sizeof(int) * (biggest + 1));
 	index = malloc(sizeof(int) * (biggest + 1));
 
+	for (i = 0; i < biggest; i++)
+		aux[i] = 0;
 	for (j = 0; j <= biggest; j++)
 	{
 		size_i += 1;
@@ -29,12 +32,11 @@ void counting_sort(int *array, size_t size)
 			if (array[i] == j)
 			{
 				count += 1;
-				break;
 			}
 		}
-		index[j] = count;
+		aux[j] = count;
 	}
-	print_array(index, size_i);
+	print_array(aux, size_i);
 	for (k = 1; k < size_i; k++)
 	{
 		if (index[k] < index[k + 1])
