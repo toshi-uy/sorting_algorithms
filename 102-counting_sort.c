d81fe9bbdd34c3d22e271815e6766765bb2c57a8
RaimundoGallino <raigallino0@gmail.com>
2021-06-16 14:09:31 -0300

    added  printing

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,9 +4,8 @@
 /**
  * counting sort - this is the Counting sort algorithm
  * implementation in C
- * @list: pointer to list
- * @min: min node value of the list
- * @max: mmax value of the list
+ * @array: array of integers
+ * @size: size of array
  * Return: no-return (void function)
  */
 
@@ -15,12 +14,11 @@ void counting_sort(int *array, size_t size)
 	unsigned int i;
 	int j, k, size_i = 0, *index, biggest = 0, count = 0;
 
-
 	for (i = 0; i < size; i++)
 	{
 		if (biggest < array[i])
 			biggest = array[i];
-	}	
+	}
 	index = malloc(sizeof(int) * (biggest + 2));
 
 	for (j = 0; j <= biggest + 1; j++)
@@ -34,9 +32,8 @@ void counting_sort(int *array, size_t size)
 				count += 1;
 				break;
 			}
-		}		
-	}	
-	for (k = 0; k < size_i - 1; k++)
+		}		}	
+	print_array(index, size_i);	for (k = 0; k < size_i - 1; k++)
 	{
 		if (index[k] < index[k + 1])
 			array[index[k]] = k;
