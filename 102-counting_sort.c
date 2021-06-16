1e00981593c0e244b94260d0963f098398f63e7f
toshi-uy <s_borgia@hotmail.com>
2021-06-16 16:54:53 -0300

    biggest + 1

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -21,7 +21,6 @@ void counting_sort(int *array, size_t size)
 	}
 	biggest += 1;
 	index = malloc(sizeof(int) * biggest);
-
 	for (j = 0; j < biggest; j++)
 		index[j] = 0;
 	for (j = 0; j < biggest; j++)
@@ -45,7 +44,7 @@ void counting_sort(int *array, size_t size)
 	for (k = biggest - 1; k > 0; k--)
 	{
 		if (index[k] > index[k - 1])
-			while(index[k] - index[k - 1] >= 1)
+			while (index[k] - index[k - 1] >= 1)
 			{
 				array[index[k] - 1] = k;
 				index[k] -= 1;
