e8bf8e59be2eec462d097a5f36c4f6bf62357e97
RaimundoGallino <raigallino0@gmail.com>
2021-06-16 14:02:03 -0300

    uploading counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,15 +12,35 @@
 
 void counting_sort(int *array, size_t size)
 {
-    size_t i;
-
-    int *index_array = malloc()
-
-    for (i = 0; i < size; i++)
-    {   
-        
-
-    }
+	size_t i, j, k, size_i = 0;
+	int *index, biggest = 0, count = 0;
 
 
+	for (i = 0; i < size; i++)
+	{
+		if (biggest < array[i])
+			biggest = array[i];
+	}	
+	index = malloc(sizeof(int) * (biggest + 2));
+
+	
+	for (j = 0; j <= biggest; j++)
+	{
+		size_i += 1;
+		index[j] = count;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] == j)
+			{
+				count += 1;
+				break;
+			}
+		}		
+	}	
+	for (k = 0; k < size_i - 1; k++)
+	{
+		if (index[k] < index[k + 1])
+			array[index[k]] = k;
+	}
+	free(index);
 }
