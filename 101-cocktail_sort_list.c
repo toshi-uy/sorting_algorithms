96af53695eb4b50973f480f6d82ade798b780dea
toshi-uy <s_borgia@hotmail.com>
2021-06-16 13:16:48 -0300

    checking back

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -22,14 +22,14 @@ void recursive_func_forward(listint_t *min, listint_t *max, listint_t **list)
 		{
 			if (h->next == max)
 			{
-				swap_forward(h, &*list);
+				swap_forward(h, list);
 				print_list(*list);
 				no_swap = 0;
 				break;
 			}
 			else
 			{
-				swap_forward(h, &*list);
+				swap_forward(h, list);
 				print_list(*list);
 				no_swap = 0;
 			}
@@ -67,14 +67,14 @@ void recursive_func_back(listint_t *min, listint_t *max, listint_t **list)
 		{
 			if (h->prev == min)
 			{
-				swap_back(h, &*list);
+				swap_back(h, list);
 				print_list(*list);
 				no_swap = 0;
 				break;
 			}
 			else
 			{
-				swap_back(h, &*list);
+				swap_back(h, list);
 				print_list(*list);
 				no_swap = 0;
 			}
@@ -108,7 +108,7 @@ void cocktail_sort_list(listint_t **list)
 	{
 		if (h->n > h->next->n)
 		{
-			swap_forward(h, &*list);
+			swap_forward(h, list);
 			print_list(*list);
 			no_swap = 0;
 		}
